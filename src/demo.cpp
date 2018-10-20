2fe2d5203c6d1d8a9961a0e87d60231c70e1e7fe
yukke42 <muramatsuyusuke9542@gmail.com>
2018-10-21 01:30:52 +0900

    update: separate draw coordinate axis

--- src/demo.cpp
+++ src/demo.cpp
@@ -4,12 +4,11 @@ demo.cpp
 
 #include "demo.hpp"
 
-#include <window.hpp>
+#include "window.hpp"
 
 int main()
 {
     Window window;
-
     window.show();
 
     return 0;
--- src/window.hpp
+++ src/window.hpp
@@ -2,8 +2,12 @@
 #define WINDOW_CPP
 
 #define WINDOW_NAME "demo"
-#define WINDOW_HEIGHT 500
-#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 600
+#define WINDOW_X_CENTER WINDOW_WIDTH / 2
+#define WINDOW_Y_CENTER WINDOW_HEIGHT - 100
+
+#define BLACK cv::Scalar(0, 0, 0)
 
 #include <opencv2/opencv.hpp>
 
@@ -14,7 +18,7 @@ class Window
   public:
     Window()
     {
-        window = cv::Mat::zeros(500, 500, CV_8UC3);
+        window = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC3);
         Window::refresh();
     };
     void refresh();
@@ -23,15 +27,20 @@ class Window
 
 void Window::refresh()
 {
+    // fill a window white
     for (int j = 0; j < window.rows; j++)
     {
         for (int i = 0; i < window.cols; i++)
         {
-            window.at<cv::Vec3b>(j, i)[0] = 255; // blue
-            window.at<cv::Vec3b>(j, i)[1] = 255; // green
-            window.at<cv::Vec3b>(j, i)[2] = 255; // red
+            window.at<cv::Vec3b>(j, i)[0] = 255;
+            window.at<cv::Vec3b>(j, i)[1] = 255;
+            window.at<cv::Vec3b>(j, i)[2] = 255;
         }
     }
+
+    // draw yz-coordinate axis
+    cv::line(window, cv::Point(0, WINDOW_Y_CENTER), cv::Point(WINDOW_WIDTH, WINDOW_Y_CENTER), BLACK);
+    cv::line(window, cv::Point(WINDOW_X_CENTER, 0), cv::Point(WINDOW_X_CENTER, WINDOW_HEIGHT), BLACK);
 }
 
 void Window::show()
